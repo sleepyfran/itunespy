4bcac7c188d847c20ac16c211462c40c87b72c9d
Spaceisstrange <zox198@gmail.com>
2015-07-24 16:26:42 +0300

    Setup updated

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
 
-setup(
-    name='itunespy',
-    version='1.0',
-    description='A simple library to fetch data from the iTunes Store API made for Python 3.X',
-    author="Fran González (@spaceisstrange)",
-    author_email="zox198@gmail.com",
-    url='https://github.com/spaceisstrange/itunespy',
-    download_url='https://github.com/spaceisstrange/itunespy/releases',
-    install_requires=['requests>=1.0'],
-    license='LICENSE.txt',
-    packages=['itunespy'])
+setup(name='itunespy',
+      version='1.0',
+      description='A simple library to fetch data from the iTunes Store API made for Python 3.X',
+      author='Fran González (@spaceisstrange)',
+      author_email='zox198@gmail.com',
+      license='LICENSE.txt',
+      packages=['itunespy'],
+      zip_safe=False,
+      url='https://github.com/spaceisstrange/itunespy')
