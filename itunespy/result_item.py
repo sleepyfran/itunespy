714a9707e5dc1480695c8ebe6737f101358cdb95
spaceisstrange <zox198@gmail.com>
2015-08-22 12:52:25 +0300

    Small update

--- itunespy/result_item.py
+++ itunespy/result_item.py
@@ -11,8 +11,6 @@
 # You should have received a
 #  copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>
-from avahi import string_array_to_txt_array
-
 
 class ResultItem(object):
     def __init__(self, json):
