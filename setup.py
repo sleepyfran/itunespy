c73be029d4ab855dea6200b35091d85fedd023e5
spaceisstrange <fgonzaleva@gmail.com>
2018-04-17 16:45:42 +0200

    Mail + License change

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='itunespy',
-    version='1.5.3',
+    version='1.5.5',
     description='A simple library to fetch data from the iTunes Store API made for Python 3.X',
     author='Fran González (@spaceisstrange)',
-    author_email='zox198@gmail.com',
+    author_email='fgonzaleva@gmail.com',
     url='http://github.com/spaceisstrange/itunespy',
     packages=['itunespy'],
-    install_requires=['requests>=2.8']
+    install_requires=['requests>=2.8'],
+    license='MIT'
 )
