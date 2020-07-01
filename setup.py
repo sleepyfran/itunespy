389785779cad8ca1e5ecb0221875a34a5dfa38a7
Fran González <fgonzaleva@icloud.com>
2020-07-01 09:58:32 +0200

    Update old mail and URL in setup

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='itunespy',
     version='1.6.0',
     description='A simple library to fetch data from the iTunes Store API made for Python 3.X',
-    author='Fran González (@spaceisstrange)',
-    author_email='fgonzaleva@gmail.com',
-    url='http://github.com/spaceisstrange/itunespy',
+    author='Fran González (@fgzv)',
+    author_email='fgzv@outlook.com',
+    url='http://github.com/fgzv/itunespy',
     packages=['itunespy'],
     install_requires=['requests>=2.8', 'pycountry>=19.8'],
     extras_require={
