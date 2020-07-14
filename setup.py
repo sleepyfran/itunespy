fce6ef62c5ac161ba9ec848a80c708a8ecd1a4ce
Fran González <fgonzaleva@icloud.com>
2020-07-14 16:28:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='itunespy',
     version='1.6.0',
     description='A simple library to fetch data from the iTunes Store API made for Python 3.X',
-    author='Fran González (@fgzv)',
+    author='Fran González (@sleepyfran)',
     author_email='fgzv@outlook.com',
-    url='http://github.com/fgzv/itunespy',
+    url='http://github.com/sleepyfran/itunespy',
     packages=['itunespy'],
     install_requires=['requests>=2.8', 'pycountry>=19.8'],
     extras_require={
